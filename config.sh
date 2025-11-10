100d90c1777f5725c9d1059569e68c7e19d922e9
Miguel-Machado85 <miguel.machado8506@gmail.com>
2025-11-10 17:42:29 -0500

    primer commit

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e  # para detenerse si hay errores
+
+cd /home/ubuntu/estudioFinal
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+sudo cp fastapi_srv.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable fastapi_srv.service
+sudo systemctl start fastapi_srv.service
